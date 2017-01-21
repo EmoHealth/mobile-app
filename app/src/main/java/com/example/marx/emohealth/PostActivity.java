a89e6ac92a8280c386a40a3a8e429539b108e973
Low Jian Sheng <lowjiansheng@hotmail.sg>
2017-01-21 17:06:29 +0800

    Add PostActivity

--- app/src/main/java/com/example/marx/emohealth/PostActivity.java
+++ app/src/main/java/com/example/marx/emohealth/PostActivity.java
@@ -1,15 +1,13 @@
 package com.example.marx.emohealth;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class PostActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, PostActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_post);
     }
 }
