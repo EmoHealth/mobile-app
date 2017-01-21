f070e45618f06143138ed9dc31fe12221065d99a
marxlow <marx_low94@hotmail.com>
2017-01-21 20:49:56 +0800

    Create expandable list

--- app/src/main/java/com/example/marx/emohealth/stats.java
+++ app/src/main/java/com/example/marx/emohealth/stats.java
@@ -6,24 +6,51 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class stats extends AppCompatActivity {
+
+    private ExpandableListView listView;
+    private ExpandableListAdapter listAdapter;
+    private List<String> listdataHeader;
+    private HashMap<String, List<String>> listHash;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        listView = (ExpandableListView) findViewById(R.id.expandList);
+        initData();
+        listAdapter = new ExpandableListAdapter(this,listdataHeader,listHash);
+        listView.setAdapter(listAdapter);
+
+    }
+
+    private void initData() {
+        listdataHeader = new ArrayList<>();
+        listHash = new HashMap<>();
+
+        listdataHeader.add("Current Week");
+        listdataHeader.add("Previous Week");
+
+        List<String> dev = new ArrayList<>();
+        dev.add("data data data");
+        dev.add("data ");
+
+        List<String> androidStudio = new ArrayList<>();
+        androidStudio.add("data ");
+        androidStudio.add("data ");
+        androidStudio.add("data ");
+
+        listHash.put(listdataHeader.get(0), dev);
+        listHash.put(listdataHeader.get(1), androidStudio);
+
     }
 
 }
