be16784f1dc72be6184d73020d4a338fc1ab6843
Seow Weijie <sweijie94@gmail.com>
2017-01-22 02:06:10 +0800

    working jiansheng's copy and sweijie's copy

---
+++ app/src/main/java/com/example/marx/emohealth/customlistadapter/CustomListAdapter.java
@@ -0,0 +1,46 @@
+package com.example.marx.emohealth.customlistadapter;
+
+/**
+ * Created by weijieseow on 22/1/17.
+ */
+
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CustomListAdapter extends ArrayAdapter<String> {
+
+    private final Activity context;
+    private final String[] itemname;
+    private final Integer[] imgid;
+
+    public CustomListAdapter(Activity context, String[] itemname, Integer[] imgid) {
+        super(context, R , itemname);
+        // TODO Auto-generated constructor stub
+
+        this.context = context;
+        this.itemname = itemname;
+        this.imgid = imgid;
+    }
+
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView = inflater.inflate(R.layout.mylist, null, true);
+
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.item);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+        TextView extratxt = (TextView) rowView.findViewById(R.id.textView1);
+
+        txtTitle.setText(itemname[position]);
+        imageView.setImageResource(imgid[position]);
+        return rowView;
+
+    }
+
+    ;
+}
